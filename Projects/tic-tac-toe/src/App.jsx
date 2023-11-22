77b002b175d8b8e381c84bf4ef1109fb41164e2a
Cyril Marois <cyrilmarois@gmail.com>
2023-11-22 15:24:27 +0100

    feat(scoreboard): Display static scoreboard

--- Projects/tic-tac-toe/src/App.jsx
+++ Projects/tic-tac-toe/src/App.jsx
@@ -1,5 +1,28 @@
 function App() {
-  return;
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <li>
+            <span className='player'>
+              <span className='player-name'>Player 1</span>
+              <span className='player-symbol'>X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+
+          <li>
+            <span className='player'>
+              <span className='player-name'>Player 2</span>
+              <span className='player-symbol'>O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+      </div>
+      GAME BOARD
+    </main>
+  );
 }
 
 export default App;
