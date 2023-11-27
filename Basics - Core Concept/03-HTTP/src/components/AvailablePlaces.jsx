e0cfbdf6b140dad40d7d04571c6b3fe757b6b225
Cyril Marois <cyrilmarois@gmail.com>
2023-11-27 13:28:25 +0100

    feat(loading): added loading state while fetching data

--- Basics - Core Concept/03-HTTP/src/components/AvailablePlaces.jsx
+++ Basics - Core Concept/03-HTTP/src/components/AvailablePlaces.jsx
@@ -3,17 +3,22 @@ import Places from './Places.jsx';
 import axios from 'axios';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       // const response = await fetch('http://localhost:3000/places');
       // console.log(response);
       // const data = await response.json();
       // setAvailablePlaces(data.places);
       const response = await axios.get('http://localhost:3000/places');
       console.log(response);
-      setAvailablePlaces(response.data.places);
+      setTimeout(() => {
+        setAvailablePlaces(response.data.places);
+        setIsFetching(false);
+      }, 1500);
     }
 
     fetchPlaces();
@@ -23,6 +28,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title='Available Places'
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText='Loading marvellous places...'
       fallbackText='No places available.'
       onSelectPlace={onSelectPlace}
     />
