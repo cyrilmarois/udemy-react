a92b221ef7569215bc540e8d24df0bc6021e92b7
Cyril Marois <cyrilmarois@gmail.com>
2023-11-27 02:34:33 +0100

    feat(http): async request

--- Basics - Core Concept/03-HTTP/src/components/AvailablePlaces.jsx
+++ Basics - Core Concept/03-HTTP/src/components/AvailablePlaces.jsx
@@ -6,25 +6,17 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    // fetch('http://localhost:3000/places')
-    //   .then((res) => {
-    //     console.log({ fetchRes: res });
-    //     return res.json();
-    //   })
-    //   .then((resData) => {
-    //     console.log({ fetchData: resData });
-    //     setAvailablePlaces(resData.places);
-    //   });
-    axios
-      .get('http://localhost:3000/places')
-      .then((res) => {
-        console.log({ axiosRes: res });
-        return res.data;
-      })
-      .then((resData) => {
-        console.log({ axiosResData: resData });
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      // const response = await fetch('http://localhost:3000/places');
+      // console.log(response);
+      // const data = await response.json();
+      // setAvailablePlaces(data.places);
+      const response = await axios.get('http://localhost:3000/places');
+      console.log(response);
+      setAvailablePlaces(response.data.places);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
