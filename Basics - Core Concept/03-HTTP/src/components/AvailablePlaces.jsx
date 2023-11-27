2a9ea55b4583ffcae41602d281cee3a3d72824f3
Cyril Marois <cyrilmarois@gmail.com>
2023-11-27 02:10:47 +0100

    feat(http): get places with api request

--- Basics - Core Concept/03-HTTP/src/components/AvailablePlaces.jsx
+++ Basics - Core Concept/03-HTTP/src/components/AvailablePlaces.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import axios from 'axios';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    // fetch('http://localhost:3000/places')
+    //   .then((res) => {
+    //     console.log({ fetchRes: res });
+    //     return res.json();
+    //   })
+    //   .then((resData) => {
+    //     console.log({ fetchData: resData });
+    //     setAvailablePlaces(resData.places);
+    //   });
+    axios
+      .get('http://localhost:3000/places')
+      .then((res) => {
+        console.log({ axiosRes: res });
+        return res.data;
+      })
+      .then((resData) => {
+        console.log({ axiosResData: resData });
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Places
       title='Available Places'
-      places={[]}
+      places={availablePlaces}
       fallbackText='No places available.'
       onSelectPlace={onSelectPlace}
     />
