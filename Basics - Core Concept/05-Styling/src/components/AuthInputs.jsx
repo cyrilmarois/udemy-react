acc374d157ee943a48f6b3829159a443eb05089d
Cyril Marois <cyrilmarois@gmail.com>
2023-11-28 21:58:27 +0100

    styling(inline style): conditional inline style example

--- Basics - Core Concept/05-Styling/src/components/AuthInputs.jsx
+++ Basics - Core Concept/05-Styling/src/components/AuthInputs.jsx
@@ -21,20 +21,21 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id='auth-inputs'>
+      <div className='controls'>
         <p>
           <label>Email</label>
           <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            type='email'
+            // className={emailNotValid ? 'invalid' : undefined}
+            style={{ backgroundColor: emailNotValid ? '#f87171' : '' }}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
           <label>Password</label>
           <input
-            type="password"
+            type='password'
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
@@ -42,11 +43,13 @@ export default function AuthInputs() {
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className='actions'>
+        <button type='button' className='text-button'>
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className='button' onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
