e0fa99a34b94a59d52b89194475f660487586058
Cyril Marois <cyrilmarois@gmail.com>
2023-11-29 21:42:03 +0100

    feat(player): output player name input

--- Basics - Core Concept/06-Advanced/src/components/Player.jsx
+++ Basics - Core Concept/06-Advanced/src/components/Player.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [inputPlayerName, setInputPlayerName] = useState('');
+  const [isButtonToggled, setIsButtonToggled] = useState(false);
+
+  function handleChangePlayerName(e) {
+    setIsButtonToggled(false);
+    setInputPlayerName(e.target.value);
+  }
+
+  function toggleClick() {
+    setIsButtonToggled(true);
+  }
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {isButtonToggled ? inputPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type='text'
+          value={inputPlayerName}
+          onChange={handleChangePlayerName}
+        />
+        <button onClick={toggleClick}>Set Name</button>
       </p>
     </section>
   );
