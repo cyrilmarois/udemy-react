9e8df02ef06c4331da7f4d56a7aeb446435ab1bf
Cyril Marois <cyrilmarois@gmail.com>
2023-11-29 21:52:06 +0100

    feat(useRef): get input using useRef

--- Basics - Core Concept/06-Advanced/src/components/Player.jsx
+++ Basics - Core Concept/06-Advanced/src/components/Player.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Player() {
+  let playerLevel = useRef();
   const [inputPlayerName, setInputPlayerName] = useState('');
+  const [inputPlayerLevel, setInputPlayerLevel] = useState(null);
   const [isButtonToggled, setIsButtonToggled] = useState(false);
 
   function handleChangePlayerName(e) {
@@ -9,20 +11,29 @@ export default function Player() {
     setInputPlayerName(e.target.value);
   }
 
-  function toggleClick() {
+  function handlePlayerNameClick() {
     setIsButtonToggled(true);
+  }
+
+  function handlePlayerLevelClick() {
+    setInputPlayerLevel(playerLevel.current.value);
   }
 
   return (
     <section id='player'>
       <h2>Welcome {isButtonToggled ? inputPlayerName : 'unknown entity'}</h2>
+      <h2>Your level is {inputPlayerLevel ?? 'to be determined'}</h2>
       <p>
         <input
           type='text'
           value={inputPlayerName}
           onChange={handleChangePlayerName}
         />
-        <button onClick={toggleClick}>Set Name</button>
+        <button onClick={handlePlayerNameClick}>Set Name</button>
+      </p>
+      <p>
+        <input ref={playerLevel} type='number' />
+        <button onClick={handlePlayerLevelClick}>Set Level</button>
       </p>
     </section>
   );
