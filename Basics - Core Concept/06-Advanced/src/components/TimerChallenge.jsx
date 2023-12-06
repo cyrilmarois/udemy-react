0342af6dab859e1b2ec5a1491961df5b2b22d88b
Cyril Marois <cyrilmarois@gmail.com>
2023-12-06 21:50:50 +0100

    feat(timerChallenge): set individual for each  cmp using ref

--- Basics - Core Concept/06-Advanced/src/components/TimerChallenge.jsx
+++ Basics - Core Concept/06-Advanced/src/components/TimerChallenge.jsx
@@ -1,14 +1,40 @@
+import { useState, useRef } from 'react';
+
 const TimerChallenge = ({ title, targetTime }) => {
+    const timer = useRef();
+    const [isTimerStarted, setIsTimerStarted] = useState(false);
+    const [isTimerExpired, setIsTimerExpired] = useState(false);
+
+    function handleStart() {
+        timer.current = setTimeout(() => {
+            setIsTimerExpired(true);
+        }, targetTime * 1000);
+
+        setIsTimerStarted(true);
+    }
+
+    function handleStop() {
+        clearTimeout(timer.current);
+    }
+
     return (
         <section className='challenge'>
             <h2>{title}</h2>
+            {isTimerExpired && <p>You Lost !</p>}
             <p className='challenge-time'>
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>Start Challenge</button>
+                {isTimerStarted && (
+                    <button onClick={handleStop}>Stop Challenge</button>
+                )}
+                {!isTimerStarted && (
+                    <button onClick={handleStart}>Start Challenge</button>
+                )}
             </p>
-            <p className='active'></p>
+            <p className={isTimerStarted ? 'active' : undefined}>
+                {isTimerStarted ? 'Time is running...' : 'Timer inactive'}
+            </p>
         </section>
     );
 };
