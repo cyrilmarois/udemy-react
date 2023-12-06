1b532bb63cc90eae7ef1038d1f169385b3cc488c
Cyril Marois <cyrilmarois@gmail.com>
2023-12-06 20:55:43 +0100

    style(project): normalize with prettier

--- Basics - Core Concept/06-Advanced/src/components/Player.jsx
+++ Basics - Core Concept/06-Advanced/src/components/Player.jsx
@@ -1,40 +1,45 @@
 import { useRef, useState } from 'react';
 
 export default function Player() {
-  let playerLevel = useRef();
-  const [inputPlayerName, setInputPlayerName] = useState('');
-  const [inputPlayerLevel, setInputPlayerLevel] = useState(null);
-  const [isButtonToggled, setIsButtonToggled] = useState(false);
+    let playerLevel = useRef();
+    const [inputPlayerName, setInputPlayerName] = useState('');
+    const [inputPlayerLevel, setInputPlayerLevel] = useState(null);
+    const [isButtonToggled, setIsButtonToggled] = useState(false);
 
-  function handleChangePlayerName(e) {
-    setIsButtonToggled(false);
-    setInputPlayerName(e.target.value);
-  }
+    function handleChangePlayerName(e) {
+        setIsButtonToggled(false);
+        setInputPlayerName(e.target.value);
+    }
 
-  function handlePlayerNameClick() {
-    setIsButtonToggled(true);
-  }
+    function handlePlayerNameClick() {
+        setIsButtonToggled(true);
+    }
 
-  function handlePlayerLevelClick() {
-    setInputPlayerLevel(playerLevel.current.value);
-  }
+    function handlePlayerLevelClick() {
+        setInputPlayerLevel(playerLevel.current.value);
+    }
 
-  return (
-    <section id='player'>
-      <h2>Welcome {isButtonToggled ? inputPlayerName : 'unknown entity'}</h2>
-      <h2>Your level is {inputPlayerLevel ?? 'to be determined'}</h2>
-      <p>
-        <input
-          type='text'
-          value={inputPlayerName}
-          onChange={handleChangePlayerName}
-        />
-        <button onClick={handlePlayerNameClick}>Set Name</button>
-      </p>
-      <p>
-        <input ref={playerLevel} type='number' />
-        <button onClick={handlePlayerLevelClick}>Set Level</button>
-      </p>
-    </section>
-  );
+    return (
+        <section id='player'>
+            <h2>
+                Welcome {isButtonToggled ? inputPlayerName : 'unknown entity'}
+            </h2>
+            <h2>Your level is {inputPlayerLevel ?? 'to be determined'}</h2>
+            <p>
+                <input
+                    type='text'
+                    value={inputPlayerName}
+                    onChange={handleChangePlayerName}
+                />
+                <button onClick={handlePlayerNameClick}>Set Name</button>
+            </p>
+            <p>
+                <input
+                    ref={playerLevel}
+                    type='number'
+                />
+                <button onClick={handlePlayerLevelClick}>Set Level</button>
+            </p>
+        </section>
+    );
 }
